091bf974352eab5a6d5d4b566c99a815a02fd1e0
Mathis Zerbib <mathis.zerbib@gmail.com>
2021-11-27 11:08:42 +0100

    fix card and layout home

--- server.js
+++ server.js
@@ -10,6 +10,58 @@ const log = console.log;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// const handleSend = (req, res) => {
+//     const secret_key = process.env.SECRET_KEY;
+//     const token = req.body.token;
+//     const url = `https://www.google.com/recaptcha/api/siteverify?secret=${secret_key}&response=${token}`;
+
+//     fetch(url, {
+//         method: 'post'
+//     })
+//         .then(response => response.json())
+//         .then(google_response => {
+//             res.json({ google_response });
+//             console.log(google_response.success)
+//             if (google_response.success === true) {
+//                 var from = req.body.email;
+//                 var to = "mathis.zerbib.webmaster@gmail.com";
+//                 var subject = req.body.need;
+//                 var text = req.body.message
+
+//                 // Step 1
+//                 let transporter = nodemailer.createTransport({
+//                     service: 'gmail',
+//                     auth: {
+//                         user: process.env.EMAIL,
+//                         pass: process.env.PASSWORD
+//                     }
+//                 });
+
+//                 // Step 2
+//                 let mailOptions = {
+//                     from: from,
+//                     to: to,
+//                     subject: subject,
+//                     text: text + "\n from: " + from
+//                 };
+
+//                 // Step 3
+//                 transporter.sendMail(mailOptions, (err, data) => {
+//                     if (err) {
+//                         return log('Error occurs');
+//                     }
+//                     return log('Email sent!!!');
+//                 });
+//             }else {
+//                 return
+//             }
+
+
+//         })
+//         .catch(error => res.json({ error }));
+       
+//     }
+
 const handleSend = (req, res) => {
     const secret_key = process.env.SECRET_KEY;
     // 
@@ -49,10 +101,10 @@ const handleSend = (req, res) => {
                     // Step 3
                     transporter.sendMail(mailOptions, (err, data) => {
                         if (err) {
-                            // res.sendFile(path.join(__dirname, '/public/error500.html'));
+                            res.sendFile(path.join(__dirname, '/public/error500.html'));
                             return log('Error occurs');
                         }
-                        // res.sendFile(path.join(__dirname, '/public/thank-you.html'));
+                        res.sendFile(path.join(__dirname, '/public/thank-you.html'));
                         return log('Email sent!!!');
                     });
                 }
